3ab8569e387ba9ad4de697f2dc8ef2f6c2cbe909
zhy <zhangyi13@cnpc.com.cn>
2022-10-24 14:13:22 +0800

    1.文件下载功能

--- src/main/java/com/itzhy/reggie/controller/CommonController.java
+++ src/main/java/com/itzhy/reggie/controller/CommonController.java
@@ -3,12 +3,17 @@ package com.itzhy.reggie.controller;
 import com.itzhy.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -46,5 +51,29 @@ public class CommonController {
             e.printStackTrace();
         }
         return R.success(fileName);
+    }
+
+    // 文件下载
+    @GetMapping("/download")
+    public void download(HttpServletResponse response, String name) {
+        // 文件读取流
+        try {
+            FileInputStream input = new FileInputStream(new File(BASE_PATH + name));
+            // 文件输出流
+            ServletOutputStream output = response.getOutputStream();
+            // 设置
+            response.setContentType("image/jpeg");
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = input.read(bytes))!= -1) {
+                output.write(bytes,0,len);
+                output.flush();
+            }
+            output.close();
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
