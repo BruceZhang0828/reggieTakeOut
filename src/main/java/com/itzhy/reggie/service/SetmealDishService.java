b0ad27a6503a443d29a25c80726609907b117394
zhy <zhangyi13@cnpc.com.cn>
2022-10-28 15:42:00 +0800

    套餐菜品数据类创建

---
+++ src/main/java/com/itzhy/reggie/service/SetmealDishService.java
@@ -0,0 +1,12 @@
+package com.itzhy.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itzhy.reggie.entity.SetmealDish;
+
+/**
+ * @author zhy
+ * @description
+ * @date 2022/10/22 14:27
+ **/
+public interface SetmealDishService extends IService<SetmealDish> {
+}
---
+++ src/main/java/com/itzhy/reggie/service/impl/SetmealServiceDishImpl.java
@@ -0,0 +1,16 @@
+package com.itzhy.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itzhy.reggie.entity.SetmealDish;
+import com.itzhy.reggie.mapper.SetmealDishMapper;
+import com.itzhy.reggie.service.SetmealDishService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zhy
+ * @description
+ * @date 2022/10/22 14:29
+ **/
+@Service
+public class SetmealServiceDishImpl extends ServiceImpl<SetmealDishMapper, SetmealDish> implements SetmealDishService {
+}
